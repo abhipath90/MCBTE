ad99ef88f5d4ad03cb34ba6723554a0c13e82e6e
DipanshuBansal <69249866+DipanshuBansal@users.noreply.github.com>
2020-08-06 16:07:02 +0530

    Update Produce_detect.m

--- src/Produce_detect.m
+++ src/Produce_detect.m
@@ -1,9 +1,9 @@
 function Detectors = Produce_detect(regions,refinement)
 % Input definition
-% region contains cuboidal regions with lower and upper bound on x,y and z
+% The simulation domain contains cuboidal regions with lower and upper bound on x, y, and z
 % coordinates
-% refinement contains vector of desired level of refinement. Each level of
-% refinement produces 8 cells for each region
+% The refinement index indicates the desired level of refinement given by 2^{3n}. Each level of
+% refinement produces 8 cells for each region.
 
 [row,~] = size(regions);
 if(row~=length(refinement))
