7199807a008dde3729504618e63208884a47c66f
DipanshuBansal <69249866+DipanshuBansal@users.noreply.github.com>
2020-08-06 15:54:23 +0530

    Update ReadFile.m

--- src/ReadFile.m
+++ src/ReadFile.m
@@ -1,8 +1,8 @@
 function file_data = ReadFile(file_name)
     
-% return filedata if file exists and is not empty
-% if file is empty it retruns empty variable
-% if file does not exists it return file name
+% return the filedata if file exists and is not empty
+% if file is empty, it retruns a empty variable
+% if file does not exists, it return the file name
     try
         file_data = load(file_name);
     catch exception
