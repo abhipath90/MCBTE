689281ac504d746e6624707ff4b08ce24d434cbb
DipanshuBansal <69249866+DipanshuBansal@users.noreply.github.com>
2020-08-06 16:15:14 +0530

    Update select_mode.m

--- src/select_mode.m
+++ src/select_mode.m
@@ -1,7 +1,7 @@
 function index = select_mode(cumul,Nmodes)
 %SELECT_MODE - This function randomly draws an index within a cumulative
 %distribution. The distribution of the indexes follows the probability
-%density function from which the cumulative distribution is generated
+%density function from which the cumulative distribution is generated.
 %   The cumulative distribution cumul must be a 1D array with increasing
 %   values. Nmodes is the length of the array.
 
