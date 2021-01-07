58113d1adacbf1f4f0fd64bba63f51e085c88747
Abhishek <abhishekpathak9066@gmail.com>
2021-01-06 20:29:31 -0500

    added example octave file in proper location

--- Octave_implementation/thin_film_100nm_ref/reflection.m
+++
@@ -1,5 +0,0 @@
-function out_vec = reflection(in_vec,n)
-
-% reflection from a plane having normal n. (any dimension will do)
-% householder transformation
-out_vec = in_vec - 2*n*(n'*in_vec)/(n'*n);
